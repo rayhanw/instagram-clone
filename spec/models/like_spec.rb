d9a05ec3d36c1401028890d385d4fe06629624b0
Rayhan Wirjowerdojo <rayhan.wirjowerdojo@gmail.com>
2020-04-02 01:12:46 +0700

    base tests for like

--- spec/models/like_spec.rb
+++ spec/models/like_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe Like, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:like) { create(:like) }
+  let(:second_like) { build(:like, user: like.user, post: like.post) }
+
+  describe "#user with #post" do
+    it 'should have a user' do
+      expect(like.user).not_to be_nil
+    end
+
+    it 'should have a post' do
+      expect(like.post).not_to be_nil
+    end
+
+    it 'should not save when a user has already liked this post' do
+      expect(second_like.valid?).to be false
+    end
+  end
 end
