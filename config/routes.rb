6e4f25eece83b065377864b965b792d07e9258a5
Rayhan Wirjowerdojo <rayhan.wirjowerdojo@gmail.com>
2020-04-01 22:58:13 +0700

    posts route

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'pages#home'
   get 'explore', to: 'pages#explore', as: :explore
+  get 'account', to: 'pages#account', as: :account
+
+  resources :posts, except: %i[index]
 end
