961495238d372dc20b9fde72a953cc689b671aec
Rayhan Wirjowerdojo <rayhan.wirjowerdojo@gmail.com>
2020-04-02 01:52:24 +0700

    remove description from posts table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_01_182949) do
+ActiveRecord::Schema.define(version: 2020_04_01_185200) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -56,7 +56,6 @@ ActiveRecord::Schema.define(version: 2020_04_01_182949) do
   end
 
   create_table "posts", force: :cascade do |t|
-    t.string "description"
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
