682e426703ed14593bff3a7d2c22ffca239816f3
Rayhan Wirjowerdojo <rayhan.wirjowerdojo@gmail.com>
2020-04-01 22:29:30 +0700

    devise params

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    # Strong params on sign up
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[username name bio website avatar])
+    # Strong params on Account update
+    devise_parameter_sanitizer.permit(:account_update, keys: %i[name bio website avatar])
+  end
 end
