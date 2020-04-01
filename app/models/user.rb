0ce64e6a6fca48d2d5952315dcf7b08ede0e1e9c
Rayhan Wirjowerdojo <rayhan.wirjowerdojo@gmail.com>
2020-04-01 22:13:56 +0700

    user has many posts

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_one_attached :avatar
+  has_many :posts
 
   validates :username, presence: true, uniqueness: { case_sensitive: false }, length: { minimum: 6 }
 end
