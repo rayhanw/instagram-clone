1bd721dd823ca032ad6576bcac970a39bf4e4f63
Rayhan Wirjowerdojo <rayhan.wirjowerdojo@gmail.com>
2020-04-02 16:02:13 +0700

    remove post description test

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -4,10 +4,4 @@ require 'rails_helper'
 
 RSpec.describe Post, type: :model do
   let(:post) { build(:post) }
-
-  describe "#description" do
-    it 'should have a description' do
-      expect(post.description).not_to be_empty
-    end
-  end
 end
