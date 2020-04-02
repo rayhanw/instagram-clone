c5c7e2b53221d28520ffceca0d86db64edb3ff79
Rayhan Wirjowerdojo <rayhan.wirjowerdojo@gmail.com>
2020-04-03 03:08:29 +0700

    user can login with username | email

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,14 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable,
+         authentication_keys: [:login]
+
+  attr_writer :login
+
+  def login
+    @login || username || email
+  end
 
   has_one_attached :avatar
   has_many :posts
@@ -11,4 +18,15 @@ class User < ApplicationRecord
 
   validates :username, presence: true, uniqueness: { case_sensitive: false }, length: { minimum: 6 }
   validates :name, presence: true
+
+  class << self
+    def find_for_database_authentication(warden_conditions)
+      conditions = warden_conditions.dup
+      if login = conditions.delete(:login)
+        where(conditions.to_h).where(["lower(username) = :value OR lower(email) = :value", value: login.downcase]).first
+      elsif conditions.has_key?(:username) || conditions.has_key?(:email)
+        where(conditions.to_h).first
+      end
+    end
+  end
 end
