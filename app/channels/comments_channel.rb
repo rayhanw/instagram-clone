d0f9fc290b052c0b4e807007f8057b5dee6bb9ab
Rayhan Wirjowerdojo <rayhan.wirjowerdojo@gmail.com>
2020-04-02 18:06:40 +0700

    Comments action cable channel

---
+++ app/channels/comments_channel.rb
@@ -0,0 +1,11 @@
+class CommentsChannel < ApplicationCable::Channel
+  def subscribed
+    post = Post.find(params[:id])
+
+    stream_for post
+  end
+
+  def unsubscribed
+    # Any cleanup needed when channel is unsubscribed
+  end
+end
