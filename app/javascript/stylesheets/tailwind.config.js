415f1f5f2db7f50cf610f41578cbaaae023fe495
Rayhan Wirjowerdojo <rayhan.wirjowerdojo@gmail.com>
2020-04-03 02:21:36 +0700

    add custom colors

--- app/javascript/stylesheets/tailwind.config.js
+++ app/javascript/stylesheets/tailwind.config.js
@@ -1,7 +1,18 @@
+const { colors } = require("tailwindcss/defaultTheme");
+
 module.exports = {
   theme: {
-    extend: {},
+    colors: {
+      ...colors,
+      purple: {
+        default: "#4d4c7d",
+        darker: "#363062",
+        light: "#827397",
+        lighter: "#d8b9c3"
+      },
+      "light-gray": "#EDF2F7"
+    }
   },
   variants: {},
-  plugins: [],
-}
+  plugins: []
+};
