f0cfece7a736d08ac239ae2688e1e05b589709da
Rayhan Wirjowerdojo <rayhan.wirjowerdojo@gmail.com>
2020-04-02 16:19:07 +0700

    Post#photos spec

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -4,4 +4,15 @@ require 'rails_helper'
 
 RSpec.describe Post, type: :model do
   let(:post) { build(:post) }
+
+  describe "#photos" do
+    it 'should be invalid if it does not have a photo' do
+      expect(post.valid?).to be false
+    end
+
+    it 'should have at least one photo' do
+      post.photos.attach(create_file_blob)
+      expect(post.photos.any?).to be true
+    end
+  end
 end
