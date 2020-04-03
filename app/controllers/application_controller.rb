690cc8cc7ae6f9da9abf2243cfe181732a7b2369
Rayhan Wirjowerdojo <rayhan.wirjowerdojo@gmail.com>
2020-04-03 12:48:35 +0700

    fix devise strong params

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,7 +9,7 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     # Strong params on sign up
-    devise_parameter_sanitizer.permit(:sign_up, keys: %i[username name bio website avatar])
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[email username name bio website avatar])
     # Strong params on Account update
     devise_parameter_sanitizer.permit(:account_update, keys: %i[name bio website avatar])
   end
