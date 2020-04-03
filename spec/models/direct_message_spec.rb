bfffe615d99376823c2eda9db30d1851b53e5199
Rayhan Wirjowerdojo <rayhan.wirjowerdojo@gmail.com>
2020-04-03 14:53:21 +0700

    tests tests tests

--- spec/models/direct_message_spec.rb
+++ spec/models/direct_message_spec.rb
@@ -2,6 +2,7 @@ require 'rails_helper'
 
 RSpec.describe DirectMessage, type: :model do
   let(:direct_message) { create(:direct_message) }
+  let(:same_authors) { build(:direct_message, sender: direct_message.sender, receiver: direct_message.receiver) }
 
   describe "#sender" do
     it 'should have a sender' do
@@ -25,6 +26,7 @@ RSpec.describe DirectMessage, type: :model do
 
   describe "#sender <-> #receiver" do
     it 'should be different sender and receiver' do
+      expect(same_authors.valid?).not_to be true
       expect(direct_message.sender).not_to eq direct_message.receiver
     end
   end
