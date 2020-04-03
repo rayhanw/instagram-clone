3cf6fc8204dd3c16af4b885f6f6889d7e1309a36
Rayhan Wirjowerdojo <rayhan.wirjowerdojo@gmail.com>
2020-04-03 14:53:03 +0700

    unique validation sender and receiver

---
+++ app/models/direct_message.rb
@@ -0,0 +1,6 @@
+class DirectMessage < ApplicationRecord
+  belongs_to :sender, class_name: "User", foreign_key: "sender_id"
+  belongs_to :receiver, class_name: "User", foreign_key: "receiver_id"
+
+  validates :sender, uniqueness: { scope: :receiver }
+end
