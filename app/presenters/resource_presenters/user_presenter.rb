f261ee4a4624022ee0a4848ba1bce780f9b2a8c9
Rayhan Wirjowerdojo <rayhan.wirjowerdojo@gmail.com>
2020-04-03 18:49:04 +0700

    add relevant methods

--- app/presenters/resource_presenters/user_presenter.rb
+++ app/presenters/resource_presenters/user_presenter.rb
@@ -1,7 +1,19 @@
 module ResourcePresenters
   class UserPresenter < ResourcePresenters::ResourcePresenterBase
-    def hi
-      @hi ||= "Hello world!"
+    def comments_count
+      @comments_count ||= user.comments.count
+    end
+
+    def likes_count
+      @likes_count ||= user.likes.count
+    end
+
+    def posts
+      @posts ||= user.posts
+    end
+
+    def posts_count
+      @posts_count ||= user.posts.count
     end
   end
 end
