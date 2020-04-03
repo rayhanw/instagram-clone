995ec4ba6b3e4639da7f25f3236474cfa78f45bc
Rayhan Wirjowerdojo <rayhan.wirjowerdojo@gmail.com>
2020-04-03 22:52:26 +0700

    remove account route

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'pages#home'
   get 'explore', to: 'pages#explore', as: :explore
-  get 'account', to: 'pages#account', as: :account
 
   resources :posts, except: %i[index] do
     resources :likes, only: %i[create]
