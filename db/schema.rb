57be6c7a64480daaea40cdde0f4b74fb9100fd48
Rayhan Wirjowerdojo <rayhan.wirjowerdojo@gmail.com>
2020-04-04 00:33:42 +0700

    messages table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_03_074129) do
+ActiveRecord::Schema.define(version: 2020_04_03_173129) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -64,6 +64,16 @@ ActiveRecord::Schema.define(version: 2020_04_03_074129) do
     t.index ["user_id"], name: "index_likes_on_user_id"
   end
 
+  create_table "messages", force: :cascade do |t|
+    t.bigint "direct_message_id", null: false
+    t.bigint "author_id", null: false
+    t.text "content", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["author_id"], name: "index_messages_on_author_id"
+    t.index ["direct_message_id"], name: "index_messages_on_direct_message_id"
+  end
+
   create_table "posts", force: :cascade do |t|
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
@@ -95,5 +105,7 @@ ActiveRecord::Schema.define(version: 2020_04_03_074129) do
   add_foreign_key "direct_messages", "users", column: "sender_id"
   add_foreign_key "likes", "posts"
   add_foreign_key "likes", "users"
+  add_foreign_key "messages", "direct_messages"
+  add_foreign_key "messages", "users", column: "author_id"
   add_foreign_key "posts", "users"
 end
