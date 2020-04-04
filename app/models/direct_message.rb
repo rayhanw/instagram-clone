87b080263f2d6ebcdfc0757eb4e919e51cccb0c0
Rayhan Wirjowerdojo <rayhan.wirjowerdojo@gmail.com>
2020-04-05 01:01:58 +0700

    dependent destroy on messages

--- app/models/direct_message.rb
+++ app/models/direct_message.rb
@@ -1,7 +1,7 @@
 class DirectMessage < ApplicationRecord
   belongs_to :sender, class_name: "User", foreign_key: "sender_id"
   belongs_to :receiver, class_name: "User", foreign_key: "receiver_id"
-  has_many :messages
+  has_many :messages, dependent: :destroy
 
   validates :sender, uniqueness: { scope: :receiver }
 
