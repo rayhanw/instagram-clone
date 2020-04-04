b601642b68210bdade1e1868105ce6e64eadfc25
Rayhan Wirjowerdojo <rayhan.wirjowerdojo@gmail.com>
2020-04-05 01:57:51 +0700

    MessagesController bootstrapped

---
+++ app/controllers/messages_controller.rb
@@ -0,0 +1,20 @@
+class MessagesController < ApplicationController
+  def create
+    @direct_message = DirectMessage.find(params[:direct_message_id])
+    @message = Message.new(message_params)
+    @message.direct_message = @direct_message
+    @message.author = current_user
+
+    if @message.save
+      redirect_to direct_message_path(@direct_message)
+    else
+      render 'direct_messages/show'
+    end
+  end
+
+  private
+
+  def message_params
+    params.require(:message).permit(:content)
+  end
+end
