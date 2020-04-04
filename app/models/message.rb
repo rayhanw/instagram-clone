d9cd912569e12c1d965e9a7e75a75db3620a8db0
Rayhan Wirjowerdojo <rayhan.wirjowerdojo@gmail.com>
2020-04-05 01:56:14 +0700

    add with author scope

--- app/models/message.rb
+++ app/models/message.rb
@@ -3,4 +3,6 @@ class Message < ApplicationRecord
   belongs_to :author, class_name: "User", foreign_key: "author_id"
 
   validates :content, presence: true, allow_blank: false
+
+  scope :with_author, -> { includes(:author) }
 end
