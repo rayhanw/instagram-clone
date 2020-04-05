71e78c1b1c23bf6a04785b3d7e1b9859e542cc84
Rayhan Wirjowerdojo <rayhan.wirjowerdojo@gmail.com>
2020-04-05 14:14:46 +0700

    user can create DMs

--- app/controllers/direct_messages_controller.rb
+++ app/controllers/direct_messages_controller.rb
@@ -10,13 +10,14 @@ class DirectMessagesController < ApplicationController
 
   def new
     @direct_message = DirectMessage.new
+    @user_list = current_user.not_conversed_with
   end
 
   def create
     @direct_message = DirectMessage.new(direct_message_params)
     @direct_message.sender = current_user
 
-    if @direct_message
+    if @direct_message.save
       redirect_to direct_message_path(@direct_message)
     else
       render :new
