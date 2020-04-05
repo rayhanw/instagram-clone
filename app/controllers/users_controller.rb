712a4fe8c6617fce78a48a3529fde88a583510bd
Rayhan Wirjowerdojo <rayhan.wirjowerdojo@gmail.com>
2020-04-06 01:07:18 +0700

    fix Feed

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,15 @@
 class UsersController < ApplicationController
   def show
-    @user = User.find_by(username: params[:username])
+    @user = if username_params == current_user.username
+              current_user
+            else
+              User.find_by(username: username_params)
+            end
+
     @presenter = @user.present
+  end
+
+  def username_params
+    params.require(:username)
   end
 end
