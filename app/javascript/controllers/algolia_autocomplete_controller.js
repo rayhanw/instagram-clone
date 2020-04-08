e363137a79cd6758a979ea450c22261ae9962373
Rayhan Wirjowerdojo <rayhan.wirjowerdojo@gmail.com>
2020-04-09 03:30:10 +0700

    insert content

--- app/javascript/controllers/algolia_autocomplete_controller.js
+++ app/javascript/controllers/algolia_autocomplete_controller.js
@@ -1,6 +1,17 @@
 import { Controller } from "stimulus";
 import algoliasearch from "algoliasearch";
-import { insertContent } from "../utils";
+
+const buildContent = ({ avatar_url, name, username }) => `
+  <div class="mb-4">
+    <a class="flex items-center" href="/users/${username}">
+      <img src="${avatar_url}" alt="avatar" class="avatar object-cover" />
+      <div class="ml-3">
+        <h4 class="font-bold">${username}</h4>
+        <span class="text-gray-700">${name}</span>
+      </div>
+    </a>
+  </div>
+`;
 
 export default class extends Controller {
   connect() {
@@ -10,10 +21,17 @@ export default class extends Controller {
     );
     this.index = this.client.initIndex("User");
     this.options = { hitsPerPage: 8, page: 0 };
+    this.container = document.querySelector(".users-container");
   }
 
   async search(event) {
     const response = await this.index.search(event.currentTarget.value);
-    response.hits.forEach(insertContent);
+    const cardContents = response.hits.map(buildContent);
+
+    this.container.innerHTML = "";
+
+    cardContents.forEach(content => {
+      this.container.insertAdjacentHTML("beforeend", content);
+    });
   }
 }
