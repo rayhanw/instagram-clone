4e1a3d2ed8bf2b1e4db8ad9b7ed2b20225c887ad
Rayhan Wirjowerdojo <rayhan.wirjowerdojo@gmail.com>
2020-04-08 16:26:38 +0700

    add class | exif based orientation

---
+++ app/javascript/packs/plugins/init_exif.js
@@ -0,0 +1,11 @@
+import piexif from "piexifjs";
+
+export const rotatePicture = (image, event) => {
+  try {
+    const exif = piexif.load(event.target.result);
+    const orientationNumber = exif["0th"][piexif.ImageIFD.Orientation];
+    image.classList.add(`orientation-${orientationNumber}`);
+  } catch (error) {
+    console.log(error);
+  }
+};
