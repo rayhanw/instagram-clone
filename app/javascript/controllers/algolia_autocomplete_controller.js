b6e70e8f4c24a9899dd194c6ab0b29c265f6754c
Rayhan Wirjowerdojo <rayhan.wirjowerdojo@gmail.com>
2020-04-09 02:44:07 +0700

    algolia search setup

--- app/javascript/controllers/algolia_autocomplete_controller.js
+++ app/javascript/controllers/algolia_autocomplete_controller.js
@@ -1,7 +1,19 @@
 import { Controller } from "stimulus";
+import algoliasearch from "algoliasearch";
+import { insertContent } from "../utils";
 
 export default class extends Controller {
-  search(event) {
-    console.log(event.target.value);
+  connect() {
+    this.client = algoliasearch(
+      document.querySelector("meta[name='algolia-app-id']").content,
+      document.querySelector("meta[name='algolia-search-only-api-key']").content
+    );
+    this.index = this.client.initIndex("User");
+    this.options = { hitsPerPage: 8, page: 0 };
+  }
+
+  async search(event) {
+    const response = await this.index.search(event.currentTarget.value);
+    response.hits.forEach(insertContent);
   }
 }
